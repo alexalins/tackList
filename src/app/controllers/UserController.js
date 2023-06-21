f124da690c8297922b8ee71a2405c875aaf27cf4
alexalins <alexa@glinsoo.com>
2023-06-21 20:32:53 -0300

    Criando save usuario

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,23 @@
+import { json } from "sequelize";
+import User from "../models/User";
+
+class UserController {
+    async store(req, res) {
+        const userExists = await User.findOne({
+            where: {email: req.body.email}
+        });
+        
+        if(userExists) {
+            return res.status(400).json({error: 'Já existe um usuario com esse e-mail.'});
+        }
+        
+        const {id, name, email} = await User.create(req.body);
+        return res.json({
+            id,
+            name,
+            email
+        });
+    }
+}
+
+export default new UserController();

fef272728e1cdaeb3d3f46bdc17b4f67f1c7437f
alexalins <alexa@glinsoo.com>
2023-06-21 20:13:08 -0300

    Ajuste na configuração do banco

--- src/config/database.js
+++ src/config/database.js
@@ -2,8 +2,14 @@ require('dotenv').config()
 
 
 module.exports = {
-    development: {
-        url: process.env.DEV_DATABASE_URL,
-        dialect: 'postgres',
-    },
+   dialect: 'postgres',
+   host: 'localhost',
+   username: process.env.DB_USER,
+   password:  process.env.DB_PASSWORD,
+   database:  process.env.DB_DATABASE,
+   define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true
+   }
 }

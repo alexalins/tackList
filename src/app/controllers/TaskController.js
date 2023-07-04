e978f17cf2498b2ce86d0442ec01656926c2a9fb
alexalins <alexa@glinsoo.com>
2023-07-04 20:45:45 -0300

    crianda update e delete de tasks

--- src/app/controllers/TaskController.js
+++ src/app/controllers/TaskController.js
@@ -29,6 +29,34 @@ class TaskController {
 
         return res.json(tasks)
     }
+
+    async update(req, res) {
+        const { task_id } = req.params;
+        const task = await Task.findByPk(task_id);
+
+        if(!task) {
+            return res.status(400).json({error: 'Tarefa não existe.'})
+        }
+        await task.update(req.body);
+
+        return res.json(task);
+    }
+
+    async delete(req, res){
+        const { task_id } = req.params;
+        const task = await Task.findByPk(task_id);
+
+        if(!task) {
+            return res.status(400).json({error: 'Tarefa não existe.'})
+        }
+
+        if(task.user_id !== req.userId) {
+            return res.status(401).json({error: 'Requisição nãp autorizada.'})
+        }
+        
+        await task.destroy();
+        return res.json({ok: true});
+    }
 }
 
 export default new TaskController();
